5f81fde4c2ac366a6bd549f4b1cbdad85989f4f9
Joshua Gonzales <codingjogo@gmail.com>
2024-11-07 21:12:46 +0800

    feat: Add HeaderBox & TotalBalanceBox

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,28 @@
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const HomePage = () => {
+  const loggedIn = { firstName: "Jogo"}
+
   return (
     <section className='home'>
-      <h1 className='text-4xl'>Home</h1>
+      <div className="home-content">
+        <header className='home-header'>
+          <HeaderBox 
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subText="Access and manage your account and transaction effeciently."
+          />
+
+          <TotalBalanceBox 
+            accounts={[]}
+            totalBanks={1}
+            titalCurrentBalance={1250}
+          />
+        </header>
+      </div>
     </section>
   )
 }
---
+++ components/TotalBalanceBox.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const TotalBalanceBox = ({
+    accounts = [],
+totalBanks,
+titalCurrentBalance,
+} : TotalBalanceBoxProps) => {
+  return (
+    <div>TotalBalanceBox</div>
+  )
+}
+
+export default TotalBalanceBox
